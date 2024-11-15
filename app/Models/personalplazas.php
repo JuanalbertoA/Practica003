179f07d6eb4e2c2887a9ad71e36276bd485f43fc
Juanalberto <L21430318@piedrasnegras.tecnm.mx>
2024-11-14 20:52:28 -0600

    Seedersrealesss

--- app/Models/personalplazas.php
+++ app/Models/personalplazas.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PersonalPlazas extends Model
+class personalplazas extends Model
 {
     use HasFactory;
 
